5dc603b4231cdcbe6ad836327306a24c1bde586c
Ling Yi-Shan <tompoo.2015@gmail.com>
2021-10-27 11:51:20 +0800

    Initial Commit

---
+++ book/bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookApp.swift
+//  book
+//
+//  Created by 凌翊瀗 on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
